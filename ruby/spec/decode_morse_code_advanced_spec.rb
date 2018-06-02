ea5b2f5ab0c6e01e58ffa8cab003b1b5e4029250
Christopher Walsh <walshification@gmail.com>
2018-06-02 15:29:20 -0500

    removes unnecessary test

--- ruby/spec/decode_morse_code_advanced_spec.rb
+++ ruby/spec/decode_morse_code_advanced_spec.rb
@@ -2,11 +2,6 @@ require 'decode_morse_code_advanced'
 require 'decode_morse_code'
 
 describe('#decodeBits') do
-  it 'decodes HEY JUDE' do
-    transmission = '1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011'
-    expect(decodeMorse(decodeBits(transmission))).to eq('HEY JUDE')
-  end
-
   it 'decodes the alphabet' do
     transmission = '111000101010100010000000'\
                    '11101110101110001010111000101000111010111010001110101110000000'\
